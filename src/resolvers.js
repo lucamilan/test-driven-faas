195a811458803ee7f55e6bcdc114fb6c4cced158
nicola baldi (@naighes) <nic.baldi@gmail.com>
2017-11-30 10:03:36 +0100

    using well-known syntax.

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,11 +1,11 @@
 module.exports = {
   Query: {
-    value(obj, { key }, context, info) {
+    value: (obj, { key }, context, info) => {
       return context.getValue(key)
     }
   },
   Mutation: {
-    set(obj, { key, value }, context, info) {
+    set: (obj, { key, value }, context, info) => {
       return context.setValue(key, value)
     }
   }
